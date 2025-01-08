bc2a741d6a7b4096dabdb1835613810a351c5220
Aditya-Proacure <aditya.umale17@gmail.com>
2025-01-08 16:44:19 +0530

    sidebar done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ['latin']})
 
@@ -25,6 +26,7 @@ export default function Rootlayout({
           <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} totalEarnings={totalEarnings}/>
           <div className="flex flex-1">
             {/*sidebar*/}
+            <Sidebar open={sidebarOpen}/>
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
             {children}
             </main>
